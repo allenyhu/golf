312a5e19b012553dc7076b704678b8b388589fe8
Allen Huang <a.y.huang10@gmail.com>
2025-01-07 21:03:15 -0800

    feat: Add create_threshold_formatting func

--- src/formatting.py
+++ src/formatting.py
@@ -1,31 +1,31 @@
 import gspread
 from gspread_formatting import *
 
-client = gspread.oauth()
-sh = client.open("Golf Tracker").worksheet('Penmar 12/01/24')
-format_range = "E:E"
 
-
-current_rules = get_conditional_format_rules(sh)
-current_rules.extend(new_rules)
-
-current_rules.save()
-
-
-def create_putting_rules():
+def create_threshold_formatting(sheet, cell_range, threshold):
     return [
         ConditionalFormatRule(
-            ranges=[GridRange.from_a1_range(format_range, sh)],
+            ranges=[GridRange.from_a1_range(cell_range, sheet)],
             booleanRule=BooleanRule(
-                condition=BooleanCondition('NUMBER_GREATER', ['2']),
+                condition=BooleanCondition('NUMBER_GREATER', [threshold]),
                 format=CellFormat(backgroundColor=Color(1, 0, 0))  # Red
             )
         ),
         ConditionalFormatRule(
-            ranges=[GridRange.from_a1_range(format_range, sh)],
+            ranges=[GridRange.from_a1_range(cell_range, sheet)],
             booleanRule=BooleanRule(
-                condition=BooleanCondition('NUMBER_LESS', ['2']),
+                condition=BooleanCondition('NUMBER_LESS', [threshold]),
                 format=CellFormat(backgroundColor=Color(0, 1, 0))  # Green
             )
         )
     ]
+
+client = gspread.oauth()
+sh = client.open("Golf Tracker").worksheet('Penmar 11/01/24')
+
+sheet_rules = get_conditional_format_rules(sh)
+
+putting_rules = create_putting_rules(sheet, "E:E", "2")
+sheet_rules.extend(putting_rules)
+
+sheet_rules.save()
