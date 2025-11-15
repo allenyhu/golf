644e8699869e2c93db1ac2f9e68ac819200e13c1
Allen Huang <allenhuang@Allens-Laptop.lan>
2025-11-15 14:11:42 -0800

    feat: Add webapp

---
+++ webapp/src/App.svelte
@@ -0,0 +1,20 @@
+<main>
+  <h1>Hello World</h1>
+</main>
+
+<style>
+  main {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    margin: 0;
+  }
+
+  h1 {
+    font-size: 4rem;
+    font-weight: bold;
+    margin: 0;
+  }
+</style>
+
